943edef4f469aea78db83af14060f2c93fdd7c5c
Daria Kalinina <lynxx91@mail.ru>
2018-02-06 15:16:21 +0300

    create accordeon

---
+++ assets/js/script_menu.js
@@ -0,0 +1,29 @@
+"use script";
+var menuArray = document.querySelectorAll('.menu__item');
+
+  menuArray[0].addEventListener('click', function () {
+    event.preventDefault();
+    console.log('click1');
+
+    menuArray[0].classList.toggle('menu__item--active');
+    menuArray[1].classList.remove('menu__item--active');
+    menuArray[2].classList.remove('menu__item--active');
+  });
+
+  menuArray[1].addEventListener('click', function () {
+    event.preventDefault();
+    console.log('click2');
+
+    menuArray[1].classList.toggle('menu__item--active');
+    menuArray[0].classList.remove('menu__item--active');
+    menuArray[2].classList.remove('menu__item--active');
+  });
+
+  menuArray[2].addEventListener('click', function () {
+    event.preventDefault();
+    console.log('click2');
+
+    menuArray[2].classList.toggle('menu__item--active');
+    menuArray[0].classList.remove('menu__item--active');
+    menuArray[1].classList.remove('menu__item--active');
+  });
--- assets/js/script_team.js
+++ assets/js/script_team.js
@@ -1,9 +1,45 @@
 "use script";
-var team = document.querySelector('.team__item');
-var teamLink = document.querySelector('.team__link');
+var teamArray = document.querySelectorAll('.team__item');
 
-teamLink.addEventListener('click', function () {
+teamArray[0].classList.add('team__item--active');
+
+teamArray[0].addEventListener('click', function () {
   event.preventDefault();
-  
-  team.classList.add('team__item--active');
+  console.log('click1');
+  var click = 'click1';
+
+  teamArray[0].classList.add('team__item--active');
+  teamArray[1].classList.remove('team__item--active');
+  teamArray[2].classList.remove('team__item--active');
+  teamArray[3].classList.remove('team__item--active');
+});
+
+teamArray[1].addEventListener('click', function () {
+  event.preventDefault();
+  console.log('click2');
+
+  teamArray[1].classList.add('team__item--active');
+  teamArray[0].classList.remove('team__item--active');
+  teamArray[2].classList.remove('team__item--active');
+  teamArray[3].classList.remove('team__item--active');
+});
+
+teamArray[2].addEventListener('click', function () {
+  event.preventDefault();
+  console.log('click3');
+
+  teamArray[2].classList.add('team__item--active');
+  teamArray[0].classList.remove('team__item--active');
+  teamArray[1].classList.remove('team__item--active');
+  teamArray[3].classList.remove('team__item--active');
+});
+
+teamArray[3].addEventListener('click', function () {
+  event.preventDefault();
+  console.log('click4');
+
+  teamArray[3].classList.add('team__item--active');
+  teamArray[0].classList.remove('team__item--active');
+  teamArray[1].classList.remove('team__item--active');
+  teamArray[2].classList.remove('team__item--active');
 });
