a9b86835158311efdd984ad7bdef5ba8662eb70a
Daria Kalinina <lynxx91@mail.ru>
2018-02-18 21:08:22 +0300

    add yandex map

--- assets/js/script_map.js
+++ assets/js/script_map.js
@@ -1,9 +1,42 @@
-var moscow_map;
+//подключение карты
+ymaps.ready(init);
+  var myMap;
 
+  function init(){
+      myMap = new ymaps.Map("map", {
+          center: [59.94, 30.32],
+          zoom: 11
+      });
 
-ymaps.ready(function () {
-    myMap = new ymaps.Map(".contact", {
-        center: [55.76, 37.64],
-        zoom: 10
-    });
-});
+      myPlacemark1 = new ymaps.Placemark([59.956841, 30.335315], {
+          hintContent: 'Mr Burgers',
+      }, {
+          iconLayout: 'default#image',
+          iconImageHref: 'assets/icons/map-marker.svg',
+          iconImageSize: [47, 57],
+          iconImageOffset: [-23, -57]
+      });
+
+      myPlacemark2 = new ymaps.Placemark([59.935776, 30.325735], {
+          hintContent: 'Mr Burgers',
+      }, {
+          iconLayout: 'default#image',
+          iconImageHref: 'assets/icons/map-marker.svg',
+          iconImageSize: [47, 57],
+          iconImageOffset: [-23, -57]
+      });
+
+      myPlacemark3 = new ymaps.Placemark([59.913997, 30.355164], {
+          hintContent: 'Mr Burgers',
+      }, {
+          iconLayout: 'default#image',
+          iconImageHref: 'assets/icons/map-marker.svg',
+          iconImageSize: [47, 57],
+          iconImageOffset: [-23, -57]
+      });
+
+      myMap.behaviors.disable('scrollZoom');
+      myMap.geoObjects.add(myPlacemark1);
+      myMap.geoObjects.add(myPlacemark2);
+      myMap.geoObjects.add(myPlacemark3);
+  }
